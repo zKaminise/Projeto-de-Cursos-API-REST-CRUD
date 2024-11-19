f590cd80b4f6fe2fabb11904a079f5a52c141078
Gabriel Misao <gabriel.misao@osf.digital>
2024-11-19 17:42:05 -0300

    Arrumando classe CursosRepository package

--- src/main/java/com/example/crudProjectRocketSeat/repositorys.java
+++
@@ -1,7 +0,0 @@
-package com.example.crudProjectRocketSeat;
-
-import com.example.crudProjectRocketSeat.entities.Cursos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface repositorys extends JpaRepository<Cursos, Long> {
-}
---
+++ src/main/java/com/example/crudProjectRocketSeat/repositorys/CursosRepository.java
@@ -0,0 +1,7 @@
+package com.example.crudProjectRocketSeat.repositorys;
+
+import com.example.crudProjectRocketSeat.entities.Cursos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursosRepository extends JpaRepository<Cursos, Long> {
+}
