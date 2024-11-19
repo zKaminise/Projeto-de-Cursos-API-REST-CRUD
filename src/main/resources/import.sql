00bc8de2fc4470506539bc09e2eaa55007b050bb
Gabriel Misao <gabriel.misao@osf.digital>
2024-11-19 17:39:44 -0300

    Incluindo seed do Banco de dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_cursos (name, categoria, active, created_at, updated_at) VALUES ('Java Básico', 'Programação', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO tb_cursos (name, categoria, active, created_at, updated_at) VALUES ('React Avançado', 'Front-End', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO tb_cursos (name, categoria, active, created_at, updated_at) VALUES ('Banco de Dados com PostgreSQL', 'Banco de Dados', false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO tb_cursos (name, categoria, active, created_at, updated_at) VALUES ('Design de Interfaces', 'UX/UI', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
